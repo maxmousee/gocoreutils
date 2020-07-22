290b1f70b6e09f29dbd56b17813b8c93d7c6373b
Natan Facchin <maxmousee@gmail.com>
2020-07-22 20:11:31 +0200

    Check if files exist and add test files

--- cat.go
+++ cat.go
@@ -6,22 +6,35 @@ import (
 	"strings"
 )
 
-func isFile(arg string) bool {
+//ArgIsFile checks if a string is potentially a file path
+func ArgIsFile(arg string) bool {
 	return !strings.HasPrefix(arg, "-")
 }
 
-func getFiles() []string {
+//FileExists checks if the file exists and user has permission to read it
+func FileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+//GetFilesToRead Returns a list of files from os.Args
+func GetFilesToRead() []string {
 	argsWithoutProg := os.Args[1:]
 	inputFiles := make([]string, len(argsWithoutProg))
 	for i, s := range argsWithoutProg {
-		if isFile(s) {
+		if ArgIsFile(s) && FileExists(s) {
 			inputFiles[i] = s
 		}
 	}
 	return inputFiles
 }
 
+// func
+
 func main() {
-	inputFiles := getFiles()
+	inputFiles := GetFilesToRead()
 	fmt.Println(inputFiles)
 }
